4b41107a961267337594d05f2649948c60f4008a
Wojciech Kowal <w.kowal202@gmail.com>
2024-04-12 14:18:05 +0200

    create base

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+    // if arg count is less than 3 -> no input and output files specified
+    if (argc < 3){
+        throw std::invalid_argument("You have to specify input and output files");
+    }
+
+    // get input and output files from args
+    std::string input_file = argv[1], output_file = argv[2];
+
+    std::cout << input_file << ' ' << output_file << '\n';
+
+    return EXIT_SUCCESS;
+}
